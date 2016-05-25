5a037ce51052e3e56fcb7c48d1da03747ecd7337
sumengyang <842298634@qq.com>
2016-05-25 14:50:50 +0800

    重新修改连接过程及释放过程

--- src/RemoteDatabase.java
+++ src/RemoteDatabase.java
@@ -8,33 +8,33 @@ import java.util.List;
 
 public class RemoteDatabase {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
-		List list = new ArrayList();
-		try {
-			jdbc();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		jdbc();
 	}
 
-	private static void jdbc() throws SQLException  {
+	private static void jdbc() {
 		Connection conn = null;
 		Statement stmt = null;
 		ResultSet rs = null;
-		
+
 		String urlRemote = "jdbc:mysql://10.107.33.36:3306/jdbctest";
 		String urlLocal = "jdbc:mysql://localhost:3306/newtest";
 		String user = "root";
 		String password = "root";
-		
+
 		String sql = "select * from student";
-		
+
 		try {
+			//注册驱动，加载想要连接的数据库的驱动到JVM
 			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection(urlRemote, user,	password);
-			stmt = conn.createStatement();			
+			conn = DriverManager.getConnection(urlRemote, user, password);
+			
+			stmt = conn.createStatement();
+			// 这两种也可以获取Statement实例
+			// PreparedStatement pstmt = conn.prepareStatement(sql) ;
+			// CallableStatement cstmt = conn.prepareCall("{CALL demoSp(? , ?)}")；
+
 			rs = stmt.executeQuery(sql);
 
 			while (rs.next()) {
@@ -45,15 +45,33 @@ public class RemoteDatabase {
 				System.out.println();
 			}
 
-		} catch (ClassNotFoundException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
-			if (stmt != null) {
-				stmt.close();
+			if(rs != null){
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
-			if(conn != null){
-				conn.close();
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		}
 
